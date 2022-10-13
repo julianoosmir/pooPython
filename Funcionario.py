b21556154a92f1d958ec01eb429282e5d3be40dc
juliano <julianoosmir@outlook.com>
2022-10-12 20:17:36 -0400

    add repr

--- Funcionario.py
+++ Funcionario.py
@@ -1,14 +1,15 @@
 from datetime import date
 
-
 class Funcionario:
     def __init__(self, codigo: int,nome:str,sobrenome:str,dataNascimento:date):
         self.codigo = codigo
         self.nome = nome
         self.sobrenome = sobrenome
         self.dataNascimento = dataNascimento
-
+    def __repr__(self):
+        return  "%s%s%s%s%s%s%s%s" % ("codigo : ",self.codigo," | nome : ",self.nome,
+            "| sobrenome : ",self.sobrenome," | data de nascimento: ",self.dataNascimento)
  
 funca = Funcionario(1,'mario','carlos',date(2000,2,3))
 
-print(funca.dataNascimento)
+print(funca)
